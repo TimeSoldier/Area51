735a29f22ac1608d5068d532d1d31a756d1434df
TimeSoldier <124135671+TimeSoldier@users.noreply.github.com>
2023-11-30 00:35:25 +0200

    Update Program.cs

--- Area51/Program.cs
+++ Area51/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace Area51
+namespace Area51
 {
     internal class Program
 
     {
 
-        const int AgentCount = 100;
+        const int AgentCount = 10;
 
         static void Main(string[] args)
         {
@@ -26,27 +26,5 @@
             
         }
     }
-    /*internal class Program
-    {
-        const int StudentCount = 100;
-
-        static void Main(string[] args)
-        {
-            Elevator bar = new Elevator();
-            List<Thread> studentThreads = new List<Thread>(StudentCount);
-            for (int i = 0; i < StudentCount; i++)
-            {
-                Agent agent = new Agent(i.ToString());
-                Thread t = new Thread(
-                    () =>
-                    {
-                        agent.PaintTheTownRed(bar);
-                    });
-                t.Start();
-                studentThreads.Add(t);
-            }
-            foreach (var t in studentThreads) t.Join();
-            Console.WriteLine($"Bar made: {bar.Turnover} tonight.");
-        }
-    }*/
+    
 }
